1c29cdc6a5ed6d85e142eee44aee549911f5c12d
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-03-19 11:58:29 -0300

    modificando o card da sessao projeto

--- assets/js/project.js
+++ assets/js/project.js
@@ -4,7 +4,7 @@ const info = [
 	{
 		'image': 'assets/img/projeto/projeto-criptdex.png',
 		'title': 'Criptdex - Decodificador',
-		'description': 'Codificado de texto utilizando um algoritimo proposto pela Alura.',
+		'description': 'Codificado de texto utilizando simples.',
 		'github_link': 'https://github.com/ygorfsguilherme/criptdex',
 		'view_app': 'https://ygorfsguilherme.github.io/criptdex/',
 	},
@@ -12,7 +12,7 @@ const info = [
 	{
 		'image': 'assets/img/projeto/projeto-jogo-da-forca.png',
 		'title': 'Jogo da forca',
-		'description': 'Jogo desenvolvido no projeto ONE - Oracle Next Education e Alura.',
+		'description': 'Para jogar sozinho ou com amigo.',
 		'github_link': 'https://github.com/ygorfsguilherme/jogo-da-forca',
 		'view_app': 'https://ygorfsguilherme.github.io/jogo-da-forca/',
 	},
@@ -20,39 +20,48 @@ const info = [
 	{
 		'image': 'assets/img/projeto/projeto-pyformail.png',
 		'title': 'Pyformail',
-		'description': 'Api de envio de formulario para o email utilizando python e flask.',
+		'description': 'Envia formulario de contato para o email',
 		'github_link': 'https://github.com/ygorfsguilherme/api-pyformail',
 		'view_app': 'https://github.com/ygorfsguilherme/api-pyformail#o-que-pyformail',
 	},
 
+	{
+		'image': 'assets/img/projeto/projeto-pyformail.png',
+		'title': 'Pyformail',
+		'description': 'Envia formulario de contato para o email',
+		'github_link': 'https://github.com/ygorfsguilherme/api-pyformail',
+		'view_app': 'https://github.com/ygorfsguilherme/api-pyformail#o-que-pyformail',
+	},
 ]
 
 function creatorCard(image, title, description, github_link, view_app){
 	const newCard = document.createElement('div');
-	newCard.classList.add('project__cards');
+	newCard.classList.add('project__card');
 
-	const content = `<div class="project__card">
-						<img class="project__card__image" src="${image}" alt="" >
+	const content = `
+					<img class="project__card__image" src="${image}" alt="" >
 
-						<div class="project__card__body">
-							<h4 class="project__card__title">
+					<div class="project__card__footer">
+
+						<div class="project__card__description">
+							<div class="project__card__title" >
 								${title}
-							</h4>
-							<p class="project__card__description">
+							</div>
+
+							<p class="project__card__subtitle">
 								${description}
 							</p>
 						</div>
-					</div>
+						
+						<div class="project__card__links">
+							<a class="project__card__link" target="_blank" href="${view_app}">
+								<img class="project__card__link__icon" src="assets/img/icon/view-app.svg" alt="">
+							</a>
 
-					<div class="project__card__coding">
-
-						<a class="project__card__more" target="_blank" href="${view_app}">
-							<img src="assets/img/icon/view-app.svg" alt="">
-						</a>
-
-						<a class="project__card__more" target="_blank" href="${github_link}">
-							<img src="assets/img/icon/coding.svg" alt="">
-						</a>	
+							<a class="project__card__link" target="_blank" href="${github_link}">
+								<img class="project__card__link__icon" src="assets/img/icon/coding.svg" alt="">
+							</a>
+						</div>
 
 					</div>`;
 
